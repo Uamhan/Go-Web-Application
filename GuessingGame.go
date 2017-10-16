59282bd27262de9b00870b02acc9d1dcc5cb73ee
Uamhan <31971768+Uamhan@users.noreply.github.com>
2017-10-16 21:29:28 +0100

    Serve a page using Bootstrap
    page is now served rather than hardcoded, relitive link to /guess added page made root resource of web application.

--- GuessingGame.go
+++ GuessingGame.go
@@ -1,17 +1,46 @@
 package main
 
 import (
-    "fmt"				
+    "fmt"	
+	"io/ioutil"	
     "net/http"			
 )
+
+type Page struct {
+    Title string
+    Body  []byte
+}
+
+func loadPage(title string) (*Page, error) {
+    filename := title + ".txt"
+    body, err := ioutil.ReadFile(filename)
+    if err != nil {
+        return nil, err
+    }
+    return &Page{Title: "Guessing Game", Body: body}, nil
+}
+
 //function handler takes a ResponseWriter and a Request asargument
 //http.ResponseWriter assembles Http servers response when written to
 //http.Request represents the clients http request
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "<h1>Guessing Game</h1>")
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/"):]
+    p, _ := loadPage(title)
+	//if no page is loaded loads home page
+	if p == nil{
+		p, _ = loadPage("index")
+	}
+    fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
 
+//func homeHandler(w http.ResponseWriter, r *http.Request) {
+ //   p, _ := loadPage("index")
+ //   fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)	
+//	}
+
+
 func main() {
-    http.HandleFunc("/", handler)  //tells the http package to handle all requests to the web root 
+	
+    http.HandleFunc("/", viewHandler)  //tells the http package to handle all requests to the web root 
     http.ListenAndServe(":8080", nil) //listens and servers requests on port :8080
 }
