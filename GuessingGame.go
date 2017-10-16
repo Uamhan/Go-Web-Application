e97eaa13b896b35fe908c35571a0fadece9a1945
Uamhan <31971768+Uamhan@users.noreply.github.com>
2017-10-16 18:53:11 +0100

    Added exercise 1 Guessing game
    Application responds with the text “Guessing game”.

---
+++ GuessingGame.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "fmt"				
+    "net/http"			
+)
+//function handler takes a ResponseWriter and a Request asargument
+//http.ResponseWriter assembles Http servers response when written to
+//http.Request represents the clients http request
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Guessing Game")
+}
+
+func main() {
+    http.HandleFunc("/", handler)  //tells the http package to handle all requests to the web root 
+    http.ListenAndServe(":8080", nil) //listens and servers requests on port :8080
+}
